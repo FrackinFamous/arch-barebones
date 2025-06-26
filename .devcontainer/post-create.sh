eff2870716cac79be7220387a2958c056f831eda
FrackinFamous <18063491+FrackinFamous@users.noreply.github.com>
2025-06-26 03:25:51 -0400

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 set -eux
+
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm git openssh curl
+
+# Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# Add nvm to .bashrc for all future shells
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
+
+# Source nvm in the current shell
 export NVM_DIR="$HOME/.nvm"
-source $NVM_DIR/nvm.sh
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Node LTS and Gemini CLI
 nvm install --lts
 nvm use --lts
 npm install -g @google/gemini-cli
